8a216185686f1ce367fde8613ca4f01a4697ca0a
dicaka <dicaka@gmail.com>
2024-07-27 17:45:30 +0700

    update massage

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app: Application = express()
 const port: number = process.env.PORT !=null ? parseInt(process.env.PORT) : 3000
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello World!')
+    res.send('Hello World 2')
 })
 
 app.listen(port, () => {
