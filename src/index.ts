de5138c17343763125effeb4e552a636ab4a9f18
dicaka <dicaka@gmail.com>
2024-07-27 20:33:06 +0700

    Setup Husky

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
-import express, { type Request, type Response, type NextFunction, type Application, application } from "express";
-import 'dotenv/config'
+import express, {
+  type Request,
+  type Response,
+  type NextFunction,
+  type Application,
+  application,
+} from "express";
+import "dotenv/config";
 
-const app: Application = express()
-const port: number = process.env.PORT !=null ? parseInt(process.env.PORT) : 3000
+const app: Application = express();
+const port: number =
+  process.env.PORT != null ? parseInt(process.env.PORT) : 3000;
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello World 2')
-})
+  res.send("Hello World 2");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`)
-})
+  console.log(`Example app listening on http://localhost:${port}`);
+});
